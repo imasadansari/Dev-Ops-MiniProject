faa0fa27fcf6fae8c360adb72a6c970759ef0d9f
imasadansari <mohammadasad.ansari@iiitb.org>
2021-03-14 03:33:09 +0530

    Added Scientific Calculator Functions file

---
+++ Scientific_Calculator.py
@@ -0,0 +1,17 @@
+import math
+
+# Functions For the operations
+
+def sqare_root(num):
+	return math.sqrt(num)
+	
+def factorial(num):
+	return math.fact(num)
+
+def natural_log(num):
+	return math.log(num)
+
+def power(num1, num2):
+	return (num1 ** num2)
+
+
