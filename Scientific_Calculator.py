8e603138d03c7b16fc8e2323e654d12e298d47c8
imasadansari <mohammadasad.ansari@iiitb.org>
2021-03-14 04:34:33 +0530

    Adding Unit test File for the scientific calculator functions

--- Scientific_Calculator.py
+++ Scientific_Calculator.py
@@ -3,14 +3,43 @@ import math
 # Functions For the operations
 
 def square_root(num):
-	return math.sqrt(num)
+	# GEtting Exception in case of taking sqare root of Negative numbers
+	if x >= 0:
+		x = math.sqrt(x)
+		return x
+	else:
+		# Prevent Exception : ValueError: math domain error
+		# Occured due to taking sqare root of negative numeber
+		# x = x
+		print("Cannot calculate Sqaure root of Negative numbers")
+		return float(x)
+
 	
 def factorial(num):
-	return math.factorial(num)
+	temp = float(x)
+	# TO prevent ValueError: factorial() not defined for negative values EXCEPTION
+	if temp >= 0:
+		# print(temp, "    ", round(temp))
+		if temp == float(round(temp)):
+			temp = math.factorial(float(temp))
+		else:
+			temp = round(temp)
+			print("Cannot take Factorial of Decimal Numbers, SO we converted It to INT")		
+	else: 
+		temp = 0.0
+		print("Factorial of negative number cannot be taken")
+	return temp
+
 
 def natural_log(num):
-	return math.log(num)
-
+	temp = float(x)
+	
+	if(temp > 0):
+		temp = math.log(float(temp))
+		#return temp
+	else:
+		temp = 0
+	return temp	
 def power(num1, num2):
 	return (num1 ** num2)
 
---
+++ test_ScientificCalculator.py
@@ -0,0 +1,44 @@
+"""
+Unit tests for the calculator library
+"""
+# Import the Scientific Calculator Function File
+
+import Scientific_Calculator
+
+
+class TestCalculator:
+
+    def test_sqrt(self):
+        assert Scientific_Calculator.square_root(196) == 14.0
+        assert Scientific_Calculator.square_root(0) == 0.0
+        assert Scientific_Calculator.square_root(-9) == -9.0
+        assert Scientific_Calculator.square_root(1) == 1
+        assert Scientific_Calculator.square_root(3) == 1.7320508075688772
+        
+    def test_factorial(self):
+        assert Scientific_Calculator.factorial(7) == 5040
+        assert Scientific_Calculator.factorial(5) == 120
+        assert Scientific_Calculator.factorial(0) == 1
+        assert Scientific_Calculator.factorial(1) == 1
+        assert Scientific_Calculator.factorial(-5) == 0.0
+        assert Scientific_Calculator.factorial(-5.5) == 0.0
+        assert Scientific_Calculator.factorial(5.5) == 6
+        
+    def test_log(self):
+    	assert Scientific_Calculator.log(0) == 0
+    	assert Scientific_Calculator.log(-1) == 0
+    	assert Scientific_Calculator.log(-10) == 0
+    	assert Scientific_Calculator.log(2.718281828459045) == 1.0
+    	assert Scientific_Calculator.log(20) == 2.995732273553991
+    	
+    def test_power(self):
+    	assert Scientific_Calculator.power(2,9) == 512
+    	assert Scientific_Calculator.power(0, 12) == 0
+    	assert Scientific_Calculator.power(20, 0) == 1
+    	assert Scientific_Calculator.power(20, 1) == 20.0
+    	assert Scientific_Calculator.power(2, 3) == 8.0
+    	assert Scientific_Calculator.power(-2,3) == -8.0
+    	assert Scientific_Calculator.power(2,-3) == 0.125
+    	assert Scientific_Calculator.power(-2, -3) == -0.125
+    	assert Scientific_Calculator.power(-2, 4) == 16.0
+    	assert Scientific_Calculator.power(1, 23) == 1.0
